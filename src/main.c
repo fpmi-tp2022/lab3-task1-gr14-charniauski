a48f9b278ce2ec8d7ea93a6325f4994a21832c0e
RETUR07 <75529203+RETUR07@users.noreply.github.com>
2022-03-16 08:44:08 +0000

    released WichIsGreater func

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+int WichIsGreater(float a, float b){
+  if (a > b) return 1;
+  return 2;
+}
+
 int main ()
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  float class1[18];
+  float class2[18];
+  for (int i = 0; i < 18; i++)scanf("%f", &class1[i]);
+  for (int i = 0; i < 18; i++)scanf("%f", &class2[i]);
 
-  return 0;
+  
 }
