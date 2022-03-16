73ae236c23b899fac16c8eb56d68688d78616b39
RETUR07 <75529203+RETUR07@users.noreply.github.com>
2022-03-16 08:51:09 +0000

    released comporator

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int WichIsGreater(float a, float b){
   if (a > b) return 1;
   return 2;
+}
+
+int comp (const void * elem1, const void * elem2) 
+{
+    int f = *((int*)elem1);
+    int s = *((int*)elem2);
+    if (f > s) return  1;
+    if (f < s) return -1;
+    return 0;
 }
 
 int main ()
@@ -11,6 +21,8 @@ int main ()
   float class2[18];
   for (int i = 0; i < 18; i++)scanf("%f", &class1[i]);
   for (int i = 0; i < 18; i++)scanf("%f", &class2[i]);
-
-  
+  qsort(class1, sizeof(class1)/sizeof(*class1), sizeof(*class1), []());
+  qsort(class2, sizeof(class2)/sizeof(*class2), sizeof(*class2));
+  printf("%d", WichIsGreater(class1[2], class2[2]));
+  return 0;
 }
